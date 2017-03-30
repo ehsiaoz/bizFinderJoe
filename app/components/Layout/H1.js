9e9cac08c672ed0f0448907e0e6a56c51c0be29f
en <ehsiaoz@h2cap.com>
2017-03-29 21:16:09 -0500

    modified css on bizcard, h1, listing

--- app/components/Layout/H1.js
+++ app/components/Layout/H1.js
@@ -5,7 +5,7 @@ class H1 extends React.Component {
   render() {
     return (
       <div className="H1">
-        <h1>{this.props.city} {this.props.category}</h1>
+        <h1 className='h1_header'>{this.props.city} {this.props.category}</h1>
       </div>
     );
   }
--- app/components/ResultsPage/BizCard.js
+++ app/components/ResultsPage/BizCard.js
@@ -12,14 +12,21 @@ class BizCard extends React.Component {
               <img className="biz-thumbnails" src={thumbnail_url ? thumbnail_url : default_image} alt="" height="225" width="225"/>
           </div>
           <div className="col-md-8">
-            <div className='row'>
+            <div className='row bizcard_title'>
               <h4><Link to={`/biz/${_id}`} >{name}</Link></h4>
             </div>
-            <div className='row desc_snippet'>
+            <div className='row biz_info'>
+              <p className="info">Address: </p>{this.props.bizData.street_address}<br/>
+              <p className="info">Phone: </p>{this.props.bizData.phone}<br/>
+              <p className="info">Website: </p>{this.props.bizData.website_url}<br/>
+            </div>
+            <div className='row desc_snippet_row'>
               {desc_snippet}
             </div>
-            <div className='row'>
-              <a className="btn btn-default" href="#">More Info</a>
+            <div className='row button_row'>
+              <div className='col-md-2 col-md-offset-8'>
+                <Link to={`/biz/${_id}`} ><a className="btn btn-default more_button">More Info</a></Link>
+              </div>
             </div>
           </div>
       </div>
